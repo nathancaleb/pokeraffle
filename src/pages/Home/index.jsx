f0d4244eefd357f9d5d5827f9af01379f9b25f87
Nathan Caleb <nathancalebss@gmail.com>
2023-07-12 04:08:08 -0300

    func button, randon number, get from API created.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,30 @@
-import { useState } from 'react'
+import { useEffect} from 'react'
 import './styles.css'
+
 
 export function Home() {
   const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState({ name:"", picture: "", number: "", type1: "", type2: ""})
+
+  function raffleNumber(){
+    let raffledNumber = Math.floor(Math.random() * 1010) + 1;
+    console.log(raffledNumber);
+    
+      fetch(`https://pokeapi.co/api/v2/pokemon/${raffledNumber}/`)
+      .then(response => response.json())
+      .then(data => {
+        setPokemon({
+          name: data.name,
+          picture: data.sprites.front_default,
+          number: data.id
+        })
+        console.log(data.types.length);
+        console.log(pokemon.name);
+        console.log(pokemon.picture);
+        console.log(pokemon.number);
+      })
+  }
+
 
   return (
     <>
@@ -16,18 +38,20 @@ export function Home() {
           <div className="appDisplay">
             <div className="widget_box">
               <div className="screen_box">
-                <div className="pokemon_image_box"></div>
+                <div className="pokemon_image_box">
+                  <img src={pokemon.picture} alt=""/>
+                </div>
                 <div className="pokemon_info_box">
-                  <div className="pokemon_name_box"></div>
-                  <div className="pokemon_number_box"></div>
+                  <div className="pokemon_name_box">{pokemon.name}</div>
+                  <div className="pokemon_number_box">{pokemon.number}</div>
                   <div className="pokemon_type_box">
-                      <div className="type_1_box"></div>
-                      <div className="type_2_box"></div>
+                      <div className="type_1_box">{pokemon.type1}</div>
+                      <div className="type_2_box">{pokemon.type2}</div>
                   </div>
                 </div>
               </div>
               <div className="button_box">
-                <div className="button">
+                <div type="button" className="button" onClick={raffleNumber}>
                   <img src="src\assets\Image\button_image.png" alt="botão cinza com desenho de uma pokebola" />
                 </div>
               </div>
