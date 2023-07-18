979c8e62ec51b8fd449cead85fc5442efebf90fd
Nathan Caleb <nathancalebss@gmail.com>
2023-07-18 02:21:24 -0300

    Make color type sys, splash screen, other adjust

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,23 +6,139 @@ export function Home() {
   const [count, setCount] = useState(0)
   const [pokemon, setPokemon] = useState({ name:"", picture: "", number: "", type:""})
   const [type2, setType2] = useState({ type2:""})
+  const [showType2, setShowType2]=useState(true);
+  const [hideSplash, setHideSplash]=useState(true);
 
   function colorTypeChange(){
+    //TYPE 1
     if(pokemon.type === "normal"){
-      document.querySelector(".type_1_box").classList.add("normal");;
+      document.querySelector(".type_1_box").classList = "type_1_box normal";
     }
-    else{
-      console.log("não faz nada");
+    else if(pokemon.type === "fighting"){
+      document.querySelector(".type_1_box").classList = "type_1_box fighting";
+    }
+    else if(pokemon.type === "flying"){
+      document.querySelector(".type_1_box").classList = "type_1_box flying";
+    }
+    else if(pokemon.type === "poison"){
+      document.querySelector(".type_1_box").classList = "type_1_box poison";
+    }
+    else if(pokemon.type === "ground"){
+      document.querySelector(".type_1_box").classList = "type_1_box ground";
+    }
+    else if(pokemon.type === "rock"){
+      document.querySelector(".type_1_box").classList = "type_1_box rock";
+    }
+    else if(pokemon.type === "bug"){
+      document.querySelector(".type_1_box").classList = "type_1_box bug";
+    }
+    else if(pokemon.type === "ghost"){
+      document.querySelector(".type_1_box").classList = "type_1_box ghost";
+    }
+    else if(pokemon.type === "steel"){
+      document.querySelector(".type_1_box").classList = "type_1_box steel";
+    }
+    else if(pokemon.type === "fire"){
+      document.querySelector(".type_1_box").classList = "type_1_box fire";
+    }
+    else if(pokemon.type === "water"){
+      document.querySelector(".type_1_box").classList = "type_1_box water";
+    }
+    else if(pokemon.type === "grass"){
+      document.querySelector(".type_1_box").classList = "type_1_box grass";
+    }
+    else if(pokemon.type === "electric"){
+      document.querySelector(".type_1_box").classList = "type_1_box electric";
+    }
+    else if(pokemon.type === "psychic"){
+      document.querySelector(".type_1_box").classList = "type_1_box psychic";
+    }
+    else if(pokemon.type === "ice"){
+      document.querySelector(".type_1_box").classList = "type_1_box ice";
+    }
+    else if(pokemon.type === "dragon"){
+      document.querySelector(".type_1_box").classList = "type_1_box dragon";
+    }
+    else if(pokemon.type === "dark"){
+      document.querySelector(".type_1_box").classList = "type_1_box dark";
+    }
+    else if(pokemon.type === "fairy"){
+      document.querySelector(".type_1_box").classList = "type_1_box fairy";
+    }
+    //TYPE 2
+    if(type2.type2 === "normal"){
+      document.querySelector(".type_2_box").classList = "type_2_box normal";
+    }
+    else if(type2.type2 === "fighting"){
+      document.querySelector(".type_2_box").classList = "type_2_box fighting";
+    }
+    else if(type2.type2 === "flying"){
+      document.querySelector(".type_2_box").classList = "type_2_box flying";
+    }
+    else if(type2.type2 === "poison"){
+      document.querySelector(".type_2_box").classList = "type_2_box poison";
+    }
+    else if(type2.type2 === "ground"){
+      document.querySelector(".type_2_box").classList = "type_2_box ground";
+    }
+    else if(type2.type2 === "rock"){
+      document.querySelector(".type_2_box").classList = "type_2_box rock";
+    }
+    else if(type2.type2 === "bug"){
+      document.querySelector(".type_2_box").classList = "type_2_box bug";
+    }
+    else if(type2.type2 === "ghost"){
+      document.querySelector(".type_2_box").classList = "type_2_box ghost";
+    }
+    else if(type2.type2 === "steel"){
+      document.querySelector(".type_2_box").classList = "type_2_box steel";
+    }
+    else if(type2.type2 === "fire"){
+      document.querySelector(".type_2_box").classList = "type_2_box fire";
+    }
+    else if(type2.type2 === "water"){
+      document.querySelector(".type_2_box").classList = "type_2_box water";
+    }
+    else if(type2.type2 === "grass"){
+      document.querySelector(".type_2_box").classList = "type_2_box grass";
+    }
+    else if(type2.type2 === "electric"){
+      document.querySelector(".type_2_box").classList = "type_2_box electric";
+    }
+    else if(type2.type2 === "psychic"){
+      document.querySelector(".type_2_box").classList = "type_2_box psychic";
+    }
+    else if(type2.type2 === "ice"){
+      document.querySelector(".type_2_box").classList = "type_2_box ice";
+    }
+    else if(type2.type2 === "dragon"){
+      document.querySelector(".type_2_box").classList = "type_2_box dragon";
+    }
+    else if(type2.type2 === "dark"){
+      document.querySelector(".type_2_box").classList = "type_2_box dark";
+    }
+    else if(type2.type2 === "fairy"){
+      document.querySelector(".type_2_box").classList = "type_2_box fairy";
     }
   }
 
+  function splashScreenTest(){
+      if(hideSplash == true){
+        setHideSplash(false);
+      }
+      else{
+      }
+  }
+
   function raffleNumber(){
+    splashScreenTest();
     let raffledNumber = Math.floor(Math.random() * 1010) + 1;
-    console.log(raffledNumber);
-    
+    setShowType2(true);
+
       fetch(`https://pokeapi.co/api/v2/pokemon/${raffledNumber}/`)
       .then(response => response.json())
       .then(data => {
+        
         setPokemon({
           name: data.name,
           picture: data.sprites.front_default,
@@ -39,19 +155,15 @@ export function Home() {
         setType2({
           type2: " "
         })
+        setShowType2(false);
       }
-
-      console.log(pokemon.name);
-      console.log(pokemon.picture);
-      console.log(pokemon.number);
-      console.log(pokemon.type);
-      console.log(type2.type2);
 
       })
   }
 
   colorTypeChange();
 
+  
   return (
     <>
       <main className='full_content'>
@@ -64,6 +176,12 @@ export function Home() {
           <div className="appDisplay">
             <div className="widget_box">
               <div className="screen_box">
+              {
+                hideSplash?<div className="splash_screen">
+                  <img src="src\assets\Image\pokeball_icon.png" alt="desenho de uma pokebola" />
+                  <label className="splash_text">clique no botao para sortear um pokémon!!!</label>
+                </div>:null
+              }
                 <div className="pokemon_image_box">
                   <img src={pokemon.picture} alt=""/>
                 </div>
@@ -83,7 +201,9 @@ export function Home() {
                       </div>
                       <div className='type_container'>
                         <label className='label_type'>Type 2:</label>
-                        <div className="type_2_box">{type2.type2}</div>
+                        {
+                          showType2?<div className="type_2_box">{type2.type2}</div>:null
+                        }
                       </div>
                   </section>
                 </div>
